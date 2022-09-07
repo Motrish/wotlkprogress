125a2c7c68527e8c49ccff513708da0c44c2da42
Motrish <nehmer.michael@gmx.de>
2022-09-07 22:57:19 +0200

    Testrelease.

---
+++ language/english.php
@@ -0,0 +1,109 @@
+<?php
+/*	Project:			EQdkp-Plus
+ *	Package:			Wotlk Progress Module - Portal
+
+ *
+ *	Copyright (C) 2021 Motrish	
+ *
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+$lang['wotlkprogress']				= 'WoW Wotlk Progress';
+$lang['wotlkprogress_name']			= 'WoW Wotlk Progress';
+$lang['wotlkprogress_desc']			= 'Stelle hier den aktuellen Raidfortschritt ein.';
+$lang['wotlkprogress_f_boss1']		= 'Naxxramas';
+$lang['wotlkprogress_f_boss2']		= 'Eye of Eternity';
+$lang['wotlkprogress_f_boss3']		= 'The Obsidian Sanctum';
+$lang['wotlkprogress_f_boss4']		= 'Ulduar';
+$lang['wotlkprogress_f_boss5']		= 'Trial of the Crusader';
+$lang['wotlkprogress_f_boss6']		= 'Onyxias Lair';
+$lang['wotlkprogress_f_boss7']		= 'Icecrown Citadel';
+$lang['wotlkprogress_f_boss8']		= 'The Ruby Sanctum';
+
+
+$lang['wotlk_notreleased']					= 'CLOSED';
+
+$lang['wotlk_clear']					= 'CLEAR';
+
+$lang['wotlk_0_1']					= '0/1';
+
+$lang['wotlk_0_2']					= '0/2';
+$lang['wotlk_1_2']					= '1/2';
+
+$lang['wotlk_0_3']					= '0/3';
+$lang['wotlk_1_3']					= '1/3';
+$lang['wotlk_2_3']					= '2/3';
+
+$lang['wotlk_0_4']					= '0/4';
+$lang['wotlk_1_4']					= '1/4';
+$lang['wotlk_2_4']					= '2/4';
+$lang['wotlk_3_4']					= '3/4';
+
+$lang['wotlk_0_5']					= '0/5';
+$lang['wotlk_1_5']					= '1/5';
+$lang['wotlk_2_5']					= '2/5';
+$lang['wotlk_3_5']					= '3/5';
+$lang['wotlk_4_5']					= '4/5';
+
+$lang['wotlk_0_6']					= '0/6';
+$lang['wotlk_1_6']					= '1/6';
+$lang['wotlk_2_6']					= '2/6';
+$lang['wotlk_3_6']					= '3/6';
+$lang['wotlk_4_6']					= '4/6';
+$lang['wotlk_5_6']					= '5/6';
+
+$lang['wotlk_0_7']					= '0/7';
+$lang['wotlk_1_7']					= '1/7';
+$lang['wotlk_2_7']					= '2/7';
+$lang['wotlk_3_7']					= '3/7';
+$lang['wotlk_4_7']					= '4/7';
+$lang['wotlk_5_7']					= '5/7';
+$lang['wotlk_6_7']					= '6/7';
+
+$lang['wotlk_0_8']					= '0/8';
+$lang['wotlk_1_8']					= '1/8';
+$lang['wotlk_2_8']					= '2/8';
+$lang['wotlk_3_8']					= '3/8';
+$lang['wotlk_4_8']					= '4/8';
+$lang['wotlk_5_8']					= '5/8';
+$lang['wotlk_6_8']					= '6/8';
+$lang['wotlk_7_8']					= '7/8';
+
+$lang['wotlk_0_9']					= '0/9';
+$lang['wotlk_1_9']					= '1/9';
+$lang['wotlk_2_9']					= '2/9';
+$lang['wotlk_3_9']					= '3/9';
+$lang['wotlk_4_9']					= '4/9';
+$lang['wotlk_5_9']					= '5/9';
+$lang['wotlk_6_9']					= '6/9';
+$lang['wotlk_7_9']					= '7/9';
+$lang['wotlk_8_9']					= '8/9';
+
+$lang['wotlk_0_10']					= '0/10';
+$lang['wotlk_1_10']					= '1/10';
+$lang['wotlk_2_10']					= '2/10';
+$lang['wotlk_3_10']					= '3/10';
+$lang['wotlk_4_10']					= '4/10';
+$lang['wotlk_5_10']					= '5/10';
+$lang['wotlk_6_10']					= '6/10';
+$lang['wotlk_7_10']					= '7/10';
+$lang['wotlk_8_10']					= '8/10';
+$lang['wotlk_9_10']					= '9/10';
+
+$lang['wotlk_0_11']					= '0/11';
+$lang['wotlk_1_11']					= '1/11';
+$lang['wotlk_2_11']					= '2/11';
+$lang['wotlk_3_11']					= '3/11';
+$lang['wotlk_4_11']					= '4/11';
+$lang['wotlk_5_11']					= '5/11';
+$lang['wotlk_6_11']					= '6/11';
+$lang['wotlk_7_11']					= '7/11';
+$lang['wotlk_8_11']					= '8/11';
+$lang['wotlk_9_11']					= '9/11';
+$lang['wotlk_10_11']				= '10/11';
+
+$lang['test']						= array('Open','DOWN');
+?>
---
+++ wotlkprogress_portal.class.php
@@ -0,0 +1,263 @@
+<?php
+/*	Project:			EQdkp-Plus
+ *	Package:			wotlk Progress Module - Portal
+
+ *
+ *	Copyright (C) 2021 Motrish	
+ *
+ */
+
+if ( !defined('EQDKP_INC') ){
+	header('HTTP/1.0 404 Not Found');exit;
+}
+
+class wotlkprogress_portal extends portal_generic {
+
+	protected static $path		= 'wotlkprogress';
+	protected static $data		= array(
+		'name'			=> 'Wotlk Progress',
+		'version'		=> '1.3',
+		'author'		=> 'Motrish',
+		'icon'			=> 'fa-code',
+		'contact'		=> 'nehmer.michael@gmx.de',
+		'description'	=> 'Shows the progress of the Wotlk Raids',
+		'multiple'		=> false,
+		'lang_prefix'	=> 'wotlkprogress_'
+	);
+
+	protected static $positions = array( 'left1', 'left2', 'right');
+	public function get_settings($state){
+		$settings	= array(
+				#Raid 1
+			'boss1'	=> array(
+				'type'		=> 'dropdown',
+				'class'		=> 'js_reload',
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),
+					'wotlk_0_15'	=> $this->user->lang('wotlk_0_15'),
+					'wotlk_1_15'	=> $this->user->lang('wotlk_1_15'),
+					'wotlk_2_15'	=> $this->user->lang('wotlk_2_15'),
+					'wotlk_3_15'	=> $this->user->lang('wotlk_3_15'),
+					'wotlk_4_15'	=> $this->user->lang('wotlk_4_15'),
+					'wotlk_5_15'	=> $this->user->lang('wotlk_5_15'),
+					'wotlk_6_15'	=> $this->user->lang('wotlk_6_15'),
+					'wotlk_7_15'	=> $this->user->lang('wotlk_7_15'),	
+					'wotlk_8_15'	=> $this->user->lang('wotlk_8_15'),		
+					'wotlk_9_15'	=> $this->user->lang('wotlk_9_15'),
+					'wotlk_10_15'	=> $this->user->lang('wotlk_10_15'),	
+					'wotlk_11_15'	=> $this->user->lang('wotlk_11_15'),	
+					'wotlk_12_15'	=> $this->user->lang('wotlk_12_15'),		
+					'wotlk_13_15'	=> $this->user->lang('wotlk_13_15'),
+					'wotlk_14_15'	=> $this->user->lang('wotlk_14_15'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+					
+				),
+			),
+				#Raid 2
+			'boss2'	=> array(
+				'type'		=> 'dropdown',
+				'class'		=> 'js_reload',			
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),
+					'wotlk_0_1'	=> $this->user->lang('wotlk_0_1'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+				),
+			),				
+				#Raid 3
+			'boss3'	=> array(
+				'type'		=> 'dropdown',	
+				'class'		=> 'js_reload',			
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),					
+					'wotlk_0_4'	=> $this->user->lang('wotlk_0_4'),
+					'wotlk_1_4'	=> $this->user->lang('wotlk_1_4'),
+					'wotlk_2_4'	=> $this->user->lang('wotlk_2_4'),
+					'wotlk_4_4'	=> $this->user->lang('wotlk_3_4'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+
+				),
+			),
+				#Raid 4
+			'boss4'	=> array(
+				'type'		=> 'dropdown',		
+				'class'		=> 'js_reload',
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),					
+					'wotlk_0_14'	=> $this->user->lang('wotlk_0_14'),
+					'wotlk_1_14'	=> $this->user->lang('wotlk_1_14'),
+					'wotlk_2_14'	=> $this->user->lang('wotlk_2_14'),
+					'wotlk_3_14'	=> $this->user->lang('wotlk_3_14'),
+					'wotlk_4_14'	=> $this->user->lang('wotlk_4_14'),
+					'wotlk_5_14'	=> $this->user->lang('wotlk_5_14'),
+					'wotlk_6_14'	=> $this->user->lang('wotlk_6_14'),
+					'wotlk_7_14'	=> $this->user->lang('wotlk_7_14'),
+					'wotlk_8_14'	=> $this->user->lang('wotlk_8_14'),
+					'wotlk_9_14'	=> $this->user->lang('wotlk_9_14'),
+					'wotlk_10_14'	=> $this->user->lang('wotlk_10_14'),
+					'wotlk_11_14'	=> $this->user->lang('wotlk_11_14'),
+					'wotlk_12_14'	=> $this->user->lang('wotlk_12_14'),
+					'wotlk_13_14'	=> $this->user->lang('wotlk_13_14'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+				),
+			),
+
+				#Raid 5 PDOK
+			'boss5'	=> array(
+				'type'		=> 'dropdown',	
+				'class'		=> 'js_reload',			
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),
+					'wotlk_0_5'	=> $this->user->lang('wotlk_0_5'),
+					'wotlk_1_5'	=> $this->user->lang('wotlk_1_5'),
+					'wotlk_2_5'	=> $this->user->lang('wotlk_2_5'),
+					'wotlk_3_5'	=> $this->user->lang('wotlk_3_5'),
+					'wotlk_4_5'	=> $this->user->lang('wotlk_4_5'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+
+				),
+			),
+			#Raid 6 Ony
+			'boss6'	=> array(
+				'type'		=> 'dropdown',	
+				'class'		=> 'js_reload',			
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),
+					'wotlk_0_1'	=> $this->user->lang('wotlk_0_1'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+				),
+			),
+			#Raid 7 ICC
+			'boss7'	=> array(
+				'type'		=> 'dropdown',	
+				'class'		=> 'js_reload',			
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),
+					'wotlk_0_9'	=> $this->user->lang('wotlk_0_9'),
+					'wotlk_1_9'	=> $this->user->lang('wotlk_1_9'),
+					'wotlk_2_9'	=> $this->user->lang('wotlk_2_9'),
+					'wotlk_3_9'	=> $this->user->lang('wotlk_3_9'),
+					'wotlk_4_9'	=> $this->user->lang('wotlk_4_9'),
+					'wotlk_5_9'	=> $this->user->lang('wotlk_5_9'),
+					'wotlk_6_9'	=> $this->user->lang('wotlk_6_9'),
+					'wotlk_7_9'	=> $this->user->lang('wotlk_7_9'),
+					'wotlk_8_9'	=> $this->user->lang('wotlk_8_9'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+				),
+			),	
+			#Raid 8 Ruby
+			'boss8'	=> array(
+				'type'		=> 'dropdown',	
+				'class'		=> 'js_reload',			
+				'options'	=> array(
+					'wotlk_notreleased'	=> $this->user->lang('wotlk_notreleased'),
+					'wotlk_0_1'	=> $this->user->lang('wotlk_0_1'),
+					'wotlk_clear'	=> $this->user->lang('wotlk_clear'),
+				),
+			),		
+		
+		);
+		
+		return $settings;
+	}
+	protected static $install	= array(
+		'autoenable'		=> '0',
+		'defaultposition'	=> 'left',
+		'defaultnumber'		=> '8',
+	);
+	
+	protected static $apiLevel = 20;
+
+	public function output() {
+		$Imagepath=$this->server_path."portal/wotlkprogress/media/images/";
+		$arrSettingsArray=array();
+		$out="<table style='width:280px;'>";
+		$actualBoss=1;		
+		while($this->config('boss'.$actualBoss)){
+			$arrSettingsArray .= $this->config('boss'.$actualBoss);
+			if($this->config('boss'.$actualBoss) == "wotlk_notreleased"){$Down="<font color='red'>".$this->user->lang('wotlk_notreleased')."</font>";}		
+						
+			if($this->config('boss'.$actualBoss) == "wotlk_clear"){$Down="<font color='lime'>".$this->user->lang('wotlk_clear')."</font>";}				
+
+			#Malygos Onyxia Rubysanctum
+			if($this->config('boss'.$actualBoss) == "wotlk_0_1"){$Down="<font color='red'>".$this->user->lang('wotlk_0_1')."</font>";}					 
+
+			#OS
+			if($this->config('boss'.$actualBoss) == "wotlk_0_4"){$Down="<font color='red'>".$this->user->lang('wotlk_0_4')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_1_4"){$Down="<font color='yellow'>".$this->user->lang('wotlk_1_4')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_2_4"){$Down="<font color='yellow'>".$this->user->lang('wotlk_2_4')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_3_4"){$Down="<font color='yellow'>".$this->user->lang('wotlk_3_4')."</font>";}											 
+				 
+			#PDK
+			if($this->config('boss'.$actualBoss) == "wotlk_0_5"){$Down="<font color='red'>".$this->user->lang('wotlk_0_5')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_1_5"){$Down="<font color='yellow'>".$this->user->lang('wotlk_1_5')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_2_5"){$Down="<font color='yellow'>".$this->user->lang('wotlk_2_5')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_3_5"){$Down="<font color='yellow'>".$this->user->lang('wotlk_3_5')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_4_5"){$Down="<font color='yellow'>".$this->user->lang('wotlk_4_5')."</font>";}					
+
+			#ICC
+			if($this->config('boss'.$actualBoss) == "wotlk_0_12"){$Down="<font color='red'>".$this->user->lang('wotlk_0_12')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_1_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_1_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_2_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_2_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_3_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_3_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_4_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_4_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_5_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_5_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_6_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_6_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_7_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_7_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_8_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_8_12')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_9_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_9_12')."</font>";}	
+			if($this->config('boss'.$actualBoss) == "wotlk_10_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_10_12')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_11_12"){$Down="<font color='yellow'>".$this->user->lang('wotlk_11_12')."</font>";}
+
+			#Ulduar
+			if($this->config('boss'.$actualBoss) == "wotlk_0_14"){$Down="<font color='red'>".$this->user->lang('wotlk_0_14')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_1_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_1_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_2_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_2_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_3_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_3_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_4_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_4_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_5_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_5_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_6_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_6_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_7_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_7_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_8_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_8_14')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_9_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_9_14')."</font>";}	
+			if($this->config('boss'.$actualBoss) == "wotlk_10_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_10_14')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_11_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_11_14')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_12_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_12_14')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_13_14"){$Down="<font color='yellow'>".$this->user->lang('wotlk_13_14')."</font>";}
+
+
+			#Naxx
+			if($this->config('boss'.$actualBoss) == "wotlk_0_15"){$Down="<font color='red'>".$this->user->lang('wotlk_0_15')."</font>";}					
+			if($this->config('boss'.$actualBoss) == "wotlk_1_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_1_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_2_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_2_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_3_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_3_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_4_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_4_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_5_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_5_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_6_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_6_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_7_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_7_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_8_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_8_15')."</font>";}					 
+			if($this->config('boss'.$actualBoss) == "wotlk_9_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_9_15')."</font>";}	
+			if($this->config('boss'.$actualBoss) == "wotlk_10_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_10_15')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_11_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_11_15')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_12_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_12_15')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_13_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_13_15')."</font>";}
+			if($this->config('boss'.$actualBoss) == "wotlk_14_15"){$Down="<font color='yellow'>".$this->user->lang('wotlk_14_15')."</font>";}
+
+
+
+			$out.="<tr style='border-bottom: 1px solid grey;background:url(".$Imagepath."".$actualBoss.".jpg);background-size:cover;'>
+			<td style='text-shadow:1px 1px 5px black;line-height:30px'>
+			<font color='white'>
+			".$this->user->lang("wotlkprogress_f_boss".$actualBoss)."
+			</font>
+			</td>
+			<td style='text-shadow:1px 1px 5px black;line-height:30px;align=center'>
+			".$Down."
+			</td>
+			</tr>";
+			$actualBoss++;
+		}		
+		$out.="</table>";
+		return $out;
+	}
+}
+?>
